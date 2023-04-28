1816d7df0df0f8581a313814f0b8edba35b0809b
henriq-pessoa <henrique.pessoa@sptech.school>
2023-04-28 13:59:25 -0300

    Alterções finais css

--- assets/scripts/scripts-login.js
+++ assets/scripts/scripts-login.js
@@ -7,8 +7,11 @@ function abrir() {
         if (email == "" && senha == "") {
             alert("Por favor, preencha todos os campos.");
             return false;
-        } else if (email && "admin@sptech.school" && senha == "admin") {
-        abrir()
+        } else if (email != "techsoja@gmail.com" && senha != "techsoja123") {
+        alert("Email ou Senha incorreto!")
+            return false;
+        }else{
+            abrir()
             return true;
         }
     }
